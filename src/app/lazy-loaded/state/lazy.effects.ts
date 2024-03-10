322447e1ca24fc6a21797671ff4e4b8598bb3ec9
Marcel von Wyl <mrvonwyl@gmail.com>
2024-03-10 10:43:23 +0100

    relative routing in functional effects

--- src/app/lazy-loaded/state/lazy.effects.ts
+++ src/app/lazy-loaded/state/lazy.effects.ts
@@ -1,19 +1,20 @@
 import { inject } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { tap } from 'rxjs';
 import { LazyActions } from './lazy.actions';
 
 export const loadTodos = createEffect(
-  (
-    actions$ = inject(Actions),
-    router = inject(Router),
-    route = inject(ActivatedRoute)
-  ) =>
+  (actions$ = inject(Actions), router = inject(Router)) =>
     actions$.pipe(
       ofType(LazyActions.navigate),
       tap(() => {
-        router.navigate(['lazy-child'], { relativeTo: route });
+        // works
+        router.navigate([router.url, 'lazy-child']);
+
+        // do not work
+        // router.navigate(['lazy-child']);
+        // router.navigate(['lazy-child'], {relativeTo: activatedRoute});
       })
     ),
   { functional: true, dispatch: false }
